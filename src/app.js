5bf6f46d0d56752c3318d9bb7728aa1ab4e8a41e
GarethBeer <gareth.beer1989@outlook.com>
2020-01-17 00:08:24 +0000

    string tests 2 and 3 complete

--- src/app.js
+++ src/app.js
@@ -7,4 +7,15 @@ app.get('/strings/hello/world', (req, res) => {
   res.json({ result: strings.sayHello('world') });
 });
 
+app.get('/strings/upper/hello', (req, res) => {
+  res.json({ result: strings.uppercase('hello') });
+});
+
+app.get('/strings/lower/HELLO', (req, res) => {
+  res.json({ result: strings.lowercase('HELLO') });
+});
+
+app.get('/strings/lower/HELLO', (req, res) => {
+  res.json({ result: strings.lowercase('HELLO') });
+});
 module.exports = app;
