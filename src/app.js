f85252fd3fd830e12a3fd0756db16d67d20eea8d
GarethBeer <gareth.beer1989@outlook.com>
2020-01-18 21:07:52 +0000

    up to not valid numbers test

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ const {
   firstCharacters,
 } = require('./lib/src/strings');
 
+const numbers = require('./lib/src/numbers');
+
 const app = express();
 
 app.get('/strings/hello/:string', (req, res) => {
@@ -26,6 +28,19 @@ app.get('/strings/first-characters/:string', (req, res) => {
     return res.json({ result: firstCharacters(req.params.string, req.query.length) });
   }
   return res.json({ result: firstCharacter(req.params.string) });
+});
+
+app.get('/numbers/add/:number1/and/:number2', (req, res) => {
+  if (typeof req.params.number1 === 'string' ||) {
+    res.json({
+      result: numbers.add(parseInt(req.params.number1, 10), parseInt(req.params.number2, 10)),
+    });
+  } else {
+    res.status(400);
+    res.json({
+      error: 'Parameters must be valid numbers.',
+    });
+  }
 });
 
 module.exports = app;
