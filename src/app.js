3f12c3fe9530e3d9b10c889f599f3ffd95d6acaa
GarethBeer <gareth.beer1989@outlook.com>
2020-01-20 13:45:18 +0000

    multiply  post res completed

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const {
   sayHello,
   uppercase,
@@ -10,6 +11,7 @@ const {
 const numbers = require('./lib/src/numbers');
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/strings/hello/:string', (req, res) => {
   res.json({ result: sayHello(req.params.string) });
@@ -31,15 +33,45 @@ app.get('/strings/first-characters/:string', (req, res) => {
 });
 
 app.get('/numbers/add/:number1/and/:number2', (req, res) => {
-  if (typeof req.params.number1 === 'string' ||) {
-    res.json({
-      result: numbers.add(parseInt(req.params.number1, 10), parseInt(req.params.number2, 10)),
-    });
-  } else {
+  const num1 = parseInt(req.params.number1, 10);
+  const num2 = parseInt(req.params.number2, 10);
+  if (!Number.isInteger(numbers.add(num1, num2))) {
     res.status(400);
     res.json({
       error: 'Parameters must be valid numbers.',
     });
+  } else {
+    res.status(200);
+    res.json({
+      result: numbers.add(num1, num2),
+    });
+  }
+});
+
+app.get('/numbers/subtract/:number1/from/:number2', (req, res) => {
+  const num1 = parseInt(req.params.number1, 10);
+  const num2 = parseInt(req.params.number2, 10);
+  const total = numbers.subtract(num2, num1);
+  if (Number.isInteger(total)) {
+    res.json({ result: numbers.subtract(num2, num1) });
+  } else {
+    res.status(400);
+    res.json({ error: 'Parameters must be valid numbers.' });
+  }
+});
+
+app.post('/numbers/multiply', (req, res) => {
+  const { a, b } = req.body;
+  const isSumANumber = numbers.multiply(parseInt(a, 10), parseInt(b, 10));
+  if (isSumANumber) {
+    res.status(200);
+    res.json({ result: isSumANumber });
+  } else if (!a || !b) {
+    res.status(400);
+    res.json({ error: 'Parameters "a" and "b" are required.' });
+  } else {
+    res.status(400);
+    res.json({ error: 'Parameters "a" and "b" must be valid numbers.' });
   }
 });
 
